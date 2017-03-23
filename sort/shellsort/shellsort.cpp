7141758cc7c4f6fd9de36c3849372785b02d323a
maxl <m18064097027@163.com>
2017-03-22 23:46:38 -0400

    add shell sort

---
+++ sort/shellsort/shellsort.cpp
@@ -0,0 +1,7 @@
+#include "shellsort.h"
+
+
+
+void ShellSort(DataType arr[], int n) {
+
+}
---
+++ sort/shellsort/shellsort.h
@@ -0,0 +1,8 @@
+#ifndef __SHELLSORT_H__
+#define __SHELLSORT_H__
+
+#include "common.h"
+
+void ShellSort(DataType arr[], int n);
+
+#endif
